5271c19da624e504b5e1f2ec135da1ac4e3016e1
sjoleee <cent7425@gmail.com>
2022-11-30 20:10:03 +0900

    pure redux : vanilla counter done

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { legacy_createStore as createStore } from "redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,25 +12,47 @@ root.render(
   </React.StrictMode>
 );
 
-let count = 0;
-
 const minus = document.getElementById("minus");
 const plus = document.getElementById("plus");
-const counter = document.querySelector("span") as HTMLSpanElement;
+const counter = document.getElementById("counter") as HTMLSpanElement;
 
-const update = () => {
-  counter.innerText = String(count);
+interface actionProps {
+  type: string;
+}
+
+// action에서 사용하는 type을 상수로 선언
+const ACTION_TYPES = {
+  plus: "plus",
+  minus: "minus",
 };
 
-const handleMinus = () => {
-  count--;
-  update();
+counter.innerText = "0";
+
+// reducer는 dispatch의 action type에 따라 state를 변경한다.
+const countModifier = (count: number = 0, action: actionProps) => {
+  switch (action.type) {
+    case ACTION_TYPES.plus:
+      return ++count;
+    case ACTION_TYPES.minus:
+      return --count;
+    default:
+      return count;
+  }
 };
 
-const handlePlus = () => {
-  count++;
-  update();
-};
+// store 선언, reducer를 인자로 받는다
+const countStore = createStore(countModifier);
 
-minus?.addEventListener("click", handleMinus);
-plus?.addEventListener("click", handlePlus);
+// subscribe는 state의 변경을 감지하여 실행된다.
+countStore.subscribe(() => {
+  counter.innerText = String(countStore.getState());
+});
+
+// dispatch는 action을 갖고 reducer를 통해 state를 변경시킨다.
+plus?.addEventListener("click", () => {
+  countStore.dispatch({ type: ACTION_TYPES.plus });
+});
+
+minus?.addEventListener("click", () => {
+  countStore.dispatch({ type: ACTION_TYPES.minus });
+});
